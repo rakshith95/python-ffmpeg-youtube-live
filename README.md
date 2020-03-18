f6c8407d25d99e1594e1cc2f0308b461fdc8ef05
Rakshith <rakshith@detecttechnologies.com>
2020-03-18 14:25:30 +0530

    Changed to ffmpeg call within the program code

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ Simple code to stream from a python program using opencv to Youtube Live using f
 
 
 * Run with: 
- `python capture.py <video path/camera device> | ffmpeg -i - -f lavfi -i anullsrc -acodec libmp3lame -ar 44100 -deinterlace -vcodec nvenc_h264 -pix_fmt yuv420p -s 1920x1080 -r 30 -g 120 -b:v 2500k -threads:6 -qscale:3 -b:a:712000 -bufsize:512k -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUR_KEY"`
+ `python capture.py <video path/camera device> <YOUTUBE_KEY>`
 
 * The python code streams to stdout buffer, which ffmpeg reads in as a pipe input.
+* Change the ffmpeg parameters in the program to suit your requirements
 * The `-f lavfi -i anullsrc -acodec libmp3lame -ar 44100` adds dummy audio to be able to stream to YouTube Live. 
 * Pipe breaks without the cv2.imencode. I don't fully understand why
--- capture.py
+++ capture.py
@@ -1,19 +1,47 @@
 import cv2
 import numpy as np 
 import sys
+import subprocess as sp 
+
+FFMPEG_BIN = "ffmpeg" # on Linux ans Mac OS
 
 video_path=sys.argv[1]
+youtube_live_key = sys.argv[2]
+
+command = [ FFMPEG_BIN,
+        '-i', '-', #input from pipe
+        '-f', 'lavfi',
+        '-i','anullsrc', #Youtube Live needs some audio, so create fake audio src
+        '-acodec', 'libmp3lame', 
+        '-ar', '44100',
+        '-deinterlace', 
+        '-vcodec', 'nvenc_h264', #Use libx264 if no nvidia GPU available
+        '-pix_fmt', 'yuv420p',
+        '-s', '1280x720',
+        '-preset', 'ultrafast',
+    '-tune', 'fastdecode',
+    '-r', '30', #Frame rate
+    '-g', '120', 
+    '-b:v', '2500k',
+    '-threads:6', 
+    '-qscale:3',
+    '-b:a:712000',
+    '-buffsize:512k',
+    '-f' , 'flv',
+    'rtmp://a.rtmp.youtube.com/live2/{}'.format(youtube_live_key),
+        ]
+
+pipe = sp.Popen( command, stdin=sp.PIPE, stderr=sp.PIPE)
 
 cap = cv2.VideoCapture(video_path)
-ret2=True
+ret2=True   
 i=1
 ret=None
 frame=None
 while True:
     ret, frame = cap.read()
     if ret:
-        # frame = cv2.resize(frame, (854,480))
-        ret2,frame2 = cv2.imencode('.jpg',frame)
+        ret2,frame_out = cv2.imencode('.jpg',frame) #Breaks without this for some reason
         if ret2:
-            sys.stdout.buffer.write(frame2.tobytes())
+	        pipe.stdin.write( frame_out.tobytes() )
             
